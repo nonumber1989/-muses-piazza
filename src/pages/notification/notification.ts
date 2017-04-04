1797d135f6c416c2563922bb479763c34b039221
Steven Xu <nonumber1989@gmail.com>
2017-04-04 22:22:55 +0800

    segment and images

--- src/pages/notification/notification.ts
+++ src/pages/notification/notification.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input  } from '@angular/core';
 
 import { ImagePicker } from 'ionic-native';
 import { NavController } from 'ionic-angular';
@@ -9,8 +9,15 @@ import { NavController } from 'ionic-angular';
 })
 export class NotificationPage {
 
+  @Input() imgs: string[];
+  notification: string;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.notification = 'message';
   }
 
   swithNotification() {
@@ -22,6 +29,7 @@ export class NotificationPage {
   for (var i = 0; i < results.length; i++) {
       console.log('Image URI: ' + results[i]);
      }
+     this.imgs = results;
     }, (err) => { 
     console.log('the err: ' + err);
     });  
