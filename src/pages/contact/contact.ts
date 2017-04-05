732bb7bd6108134b914e4e73aacd15cd109f5ac0
i325776 <steven.xu01@sap.com>
2017-04-05 18:31:58 +0800

    caman work first

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+declare var Caman: any;
+
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+	selector: 'page-contact',
+	templateUrl: 'contact.html'
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+	@ViewChild('mapCanvas') mapElement: ElementRef;
+	constructor(public navCtrl: NavController) {}
 
-  }
+	ionViewDidLoad() { 
+		let theEle = this.mapElement.nativeElement;
+		Caman('#map_canvas', function() {
+			this.brightness(50).render();
+		});
+	}
 
 }
